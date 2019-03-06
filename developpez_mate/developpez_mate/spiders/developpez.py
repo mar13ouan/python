a470ba3a2d59a8a146013baa4bceb7d30e76294a
Marouan alioui <alioui@univ-tours.local>
2019-03-06 17:33:48 +0100

    Committer: Marouan  On branch master  Your branch is up-to-date with 'origin/master'.  Changes to be committed: 	new file:   developpez_mate/.vscode/launch.json 	modified:   developpez_mate/developpez_mate/spiders/developpez.py

--- developpez_mate/developpez_mate/spiders/developpez.py
+++ developpez_mate/developpez_mate/spiders/developpez.py
@@ -1,15 +1,15 @@
 
 import scrapy
 
-class DevelopppezSpryder(scrapy.spider):
+class DevelopppezSpryder(scrapy.Spider):
     name = 'forum-python-developpez'
 
     start_urls = ['https://www.developpez.net/forums/f938/dotnet/general-dotnet/debuter/']
 
     def parse(self, response):
         for fil_discussion in response.css('#threads .inner'):
-            yield(
-                'titre': fil_discussion.css('.threadtitle a::text').extract_first()
-            ) 
+            yield()
+                
+            
                 
             
